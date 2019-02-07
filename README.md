c912c0d811244fd3dd72f0b0bf200a0971e14d06
Shashank Agrawal <shagrawal99@gmail.com>
2019-02-07 10:09:59 +0530

    Removed `READ_PHONE_STATE` permission and storing the unique id in shared preference.

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 UniqueDeviceID
 ==============
 
+## Difference from original forked version
+
+1. Removed `READ_PHONE_STATE` permission https://android-developers.googleblog.com/2018/10/providing-safe-and-secure-experience.html
+2. Using `SharedPreferences` to store the unique UUID
+
 PhoneGap / Cordova unique device id (UUID) plugin for Android, iOS and Windows Phone 8. Remains the same after app uninstall.
 
 ## Installation
--- src/android/UniqueDeviceID.java
+++ src/android/UniqueDeviceID.java
@@ -1,109 +1,56 @@
 package hu.dpal.phonegap.plugins;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.provider.Settings.Secure;
-import android.telephony.TelephonyManager;
-import android.util.Log;
-
-import java.lang.reflect.Method;
+import java.util.UUID;
 
 public class UniqueDeviceID extends CordovaPlugin {
 
-    public static final String TAG = "UniqueDeviceID";
-    public CallbackContext callbackContext;
-    public static final int REQUEST_READ_PHONE_STATE = 0;
-
-    protected final static String permission = Manifest.permission.READ_PHONE_STATE;
+    private static final String TAG = "UniqueDeviceID";
+    private CallbackContext callbackContext;
+    private static final int REQUEST_READ_PHONE_STATE = 0;
+    private static final String PREF_UNIQUE_ID = "PREF_UNIQUE_ID";
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         this.callbackContext = callbackContext;
+
         try {
             if (action.equals("get")) {
-                if(this.hasPermission(permission)){
-                    getDeviceId();
-                }else{
-                    this.requestPermission(this, REQUEST_READ_PHONE_STATE, permission);
-                }
-            }else {
+                getDeviceId();
+            } else {
                 this.callbackContext.error("Invalid action");
                 return false;
             }
-        }catch(Exception e ) {
+        } catch (Exception e) {
             this.callbackContext.error("Exception occurred: ".concat(e.getMessage()));
             return false;
         }
+
         return true;
-
     }
 
-    public void onRequestPermissionResult(int requestCode, String[] permissions,
-                                          int[] grantResults) throws JSONException {
-        if(requestCode == REQUEST_READ_PHONE_STATE){
-            getDeviceId();
-        }
-    }
-
-    protected void getDeviceId(){
+    protected void getDeviceId() {
         try {
             Context context = cordova.getActivity().getApplicationContext();
-            TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+            SharedPreferences sharedPrefs = context.getSharedPreferences(PREF_UNIQUE_ID, Context.MODE_PRIVATE);
+            String uuid = sharedPrefs.getString(PREF_UNIQUE_ID, null);
 
-            String uuid;
-            String androidID = Secure.getString(context.getContentResolver(), Secure.ANDROID_ID);
-            String deviceID = tm.getDeviceId();
-            String simID = tm.getSimSerialNumber();
-
-            if ("9774d56d682e549c".equals(androidID) || androidID == null) {
-                androidID = "";
+            if (uuid == null) {
+                uuid = UUID.randomUUID().toString();
+                SharedPreferences.Editor editor = sharedPrefs.edit();
+                editor.putString(PREF_UNIQUE_ID, uuid);
+                editor.commit();
             }
-
-            if (deviceID == null) {
-                deviceID = "";
-            }
-
-            if (simID == null) {
-                simID = "";
-            }
-
-            uuid = androidID + deviceID + simID;
-            uuid = String.format("%32s", uuid).replace(' ', '0');
-            uuid = uuid.substring(0, 32);
-            uuid = uuid.replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
 
             this.callbackContext.success(uuid);
-        }catch(Exception e ) {
+        } catch (Exception e) {
             this.callbackContext.error("Exception occurred: ".concat(e.getMessage()));
-        }
-    }
-
-    private boolean hasPermission(String permission) throws Exception{
-        boolean hasPermission = true;
-        Method method = null;
-        try {
-            method = cordova.getClass().getMethod("hasPermission", permission.getClass());
-            Boolean bool = (Boolean) method.invoke(cordova, permission);
-            hasPermission = bool.booleanValue();
-        } catch (NoSuchMethodException e) {
-            Log.w(TAG, "Cordova v" + CordovaWebView.CORDOVA_VERSION + " does not support API 23 runtime permissions so defaulting to GRANTED for " + permission);
-        }
-        return hasPermission;
-    }
-
-    private void requestPermission(CordovaPlugin plugin, int requestCode, String permission) throws Exception{
-        try {
-            java.lang.reflect.Method method = cordova.getClass().getMethod("requestPermission", org.apache.cordova.CordovaPlugin.class ,int.class, java.lang.String.class);
-            method.invoke(cordova, plugin, requestCode, permission);
-        } catch (NoSuchMethodException e) {
-            throw new Exception("requestPermission() method not found in CordovaInterface implementation of Cordova v" + CordovaWebView.CORDOVA_VERSION);
         }
     }
 }
